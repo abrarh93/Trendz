12e4be5a10aa3342c209741d23c24fb9f02244b0
abbubhai <afrasat@unomaha.edu>
2017-12-13 18:27:08 -0600

    Trendz

---
+++ js/controllers.js
@@ -0,0 +1,79 @@
+
+angular.module('myApp')
+
+// Controller
+
+.controller('VideosController', function ($scope, $http, $log, VideosService) {
+
+    init();
+
+    function init() {
+      $scope.youtube = VideosService.getYoutube();
+      $scope.results = VideosService.getResults();
+      $scope.history = VideosService.getHistory();
+    }
+
+    $scope.isAndroid = function() {
+      return ons.platform.isAndroid();
+    }
+
+    $scope.toolbarTitle = 'Search';
+    $scope.updateToolbar = function(title) {
+      $scope.toolbarTitle = title;
+    }
+
+    $scope.focusInput = function(platform) {
+      document.getElementById(platform + '-search-input').focus();
+    };
+
+    $scope.blurInput = function(platform) {
+      document.getElementById(platform + '-search-input').blur();
+    };
+
+    $scope.launch = function (video, archive) {
+      VideosService.launchPlayer(video.id, video.title);
+      if (archive) {
+      	VideosService.archiveVideo(video);
+      }
+      $log.info('Launched id:' + video.id + ' and title:' + video.title);
+    };
+
+    $scope.nextPageToken = '';
+    $scope.labelSearch = 'You haven\'t searched for any video yet!';
+    $scope.labelHistory = 'You haven\'t watched any video yet!';
+
+    $scope.loading = false;
+
+    $scope.loadMore = function(done) {
+      $scope.search(false).then(done);
+    };
+
+    $scope.search = function (isNewQuery) {
+      $scope.loading = true;
+      return $http.get('https://www.googleapis.com/youtube/v3/search', {
+        params: {
+          key: 'AIzaSyDd2iy2edgY6Ip4QO9tm4Vcwa6d8BK2n00',
+          type: 'video',
+          maxResults: '10',
+          pageToken: isNewQuery ? '' : $scope.nextPageToken,
+          part: 'id,snippet',
+          fields: 'items/id,items/snippet/title,items/snippet/description,items/snippet/thumbnails/default,items/snippet/channelTitle,nextPageToken',
+          q: this.query
+        }
+      })
+      .success( function (data) {
+        if (data.items.length === 0) {
+          $scope.label = 'No results were found!';
+        }
+        VideosService.listResults(data, $scope.nextPageToken && !isNewQuery);
+        $scope.nextPageToken = data.nextPageToken;
+        $log.info(data);
+        $scope.loading = false;
+      })
+      .error( function (e) {
+        $log.info('Search error: ', e);
+        $scope.loading = false;
+      })
+      ;
+    };
+});
